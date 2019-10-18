4639593b2917862662d85c7691dc97e28b7782b3
git <lukechaston@gmail.com>
2019-10-18 11:03:22 +0100

    began calculator function. Added get element to the forms by ID so that we can calculate from user input.

---
+++ index.js
@@ -0,0 +1,87 @@
+//Create a function that calculates the data from the user input.
+
+
+function calculate() {
+    document.getElementById('borrowing')
+    document.getElementById('salary')
+
+
+
+}
+
+
+
+
+
+//output data to the front end.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
+
+function updateDisplay () {
+    const display = document.querySelector('.calculatorScreen')
+    display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+
+const keys = document.querySelector('.calculatorKeys');
+keys .addEventListener('click', (event) =>  {
+    const { target }  = event;
+    if (!target.matches ('button')){
+        return;
+    }
+    if (target.classList.contains('operator')) {
+        console.log ('operator', target.value);
+        return;
+    }
+    if (target.classList.contains('decimal')) {
+        console.log ('decimal', target.value);
+        return;
+    }
+    if (target.classList.contains('all-clear')) {
+        console.log ('clear', target.value);
+        return;
+    }
+    console.log ('digit', target.value);
+});
+
+ */
+
+//document.getElementById('borrowAmount')
+// document.getElementById('expectedSalary')
